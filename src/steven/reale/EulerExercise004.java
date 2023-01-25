55c06aea5e5d5c90d6f193f9dbba9d6870df6aeb
Steven Reale <stevenmreale@gmail.com>
2023-01-25 13:33:06 -0500

    Exercise 4 passing all tests

--- src/steven/reale/EulerExercise004.java
+++ src/steven/reale/EulerExercise004.java
@@ -12,26 +12,13 @@ public class EulerExercise004 {
      */
 
     private static int palindrome(int input) {
-        String[] strInput = Integer.toString(input).split("");
-        int[] intArray = stringArrToIntArr(strInput);
-        int testInt = 100 * intArray[0] + 10 * intArray[1] + intArray[2];
 
-        //if given integer is already a palindrome, decrements accordingly. Then, it determines the starting 2 or 3 digits with which to construct first palindrome to test
-        if (intArray[0] == intArray[5] && intArray[1] == intArray[4] && intArray[2] == intArray[3]) {
-            testInt--; // 200 002
-        }
-
-        //main processing loop. Calls methods to 1) construct palindromes from testInt, and 2) test whether palindrome can be expressed as product of two integers.
-        int testNum;
-        while (true) {
-
-            testNum = makePalindrome(testInt);
-            if (isThreeDigitDivisible(testNum)) {
-                return testNum;
+        for (int i = input - 1; i > 100000; i--) {
+            if (isPalindrome(i) && isThreeDigitDivisible(i)) {
+                return i;
             }
-            testInt--;
         }
-
+        return -1;
     }
 
     //tests if an integer can be expressed as the product of two 3-digit integers
@@ -45,31 +32,6 @@ public class EulerExercise004 {
         return false;
     }
 
-    //constructs a 6-digit palindrome given first three digits
-    private static int makePalindrome(int startNum) { //998
-        int[] intArray = stringArrToIntArr(Integer.toString(startNum).split(""));
-        int palindrome = intArray[0] + 10 * intArray[1] + 100 * intArray[2] + 1000 * intArray[2] + 10000 * intArray[1] + 100000 * intArray[0];
-
-        return palindrome;
-    }
-
-    //given an array of integers cast as strings, returns a corresponding array of integers
-    private static int[] stringArrToIntArr(String[] inputString) {
-        int numberOfDigits = inputString.length;
-        int[] intArray = new int[numberOfDigits];
-
-        for (int i = 0; i < numberOfDigits; i++) {
-            intArray[i] = Integer.parseInt(inputString[i]);
-        }
-        return intArray;
-    }
-
-    //test if a 6-entry integer array is a palindrome
-    private static boolean isPalindrome(int[] intArray) {
-        int arrLength = intArray.length;
-        return (intArray[0] == intArray[5] && intArray[1] == intArray[4] && (intArray[2] == intArray[3]));
-    }
-
     private static boolean isPalindrome(int testInt) {
         List<Integer> intList = new ArrayList<>();
         while (testInt > 0) {
@@ -80,20 +42,7 @@ public class EulerExercise004 {
     }
 
     public static void main(String[] args) {
-        Set<Integer> palindromes = new HashSet<>();
-        //run make palindrome for all 101 < n < 999, save each to a set
-        for (int i = 100; i <= 999; i++) {
-            palindromes.add(makePalindrome(i));
-        }
-        //run a test all method which checks to see if any are missing from the set, and which ones
-        for (int i = 999999; i > 100000; i--) {
-            if (isPalindrome(i)) {
-                if (!palindromes.contains(i)) {
-                    System.out.println(i + " was not generated");
-                }
-            }
-        }
 
-        //System.out.println(palindrome(101102));
+        System.out.println(palindrome(999999));
     }
 }
