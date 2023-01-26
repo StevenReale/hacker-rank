33aedf9beb85737a824dfccafac4bca8cdfe6cc3
Steven Reale <stevenmreale@gmail.com>
2023-01-26 05:47:36 -0500

    Day 10 started

---
+++ src/steven/reale/thirtydays/Day10.java
@@ -0,0 +1,47 @@
+package steven.reale.thirtydays;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+Given a base-10 integer, n, convert it to binary (base-2). Then find and print the base-10 integer denoting the
+maximum number of consecutive 1's in n's binary representation.
+ */
+
+public class Day10 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        bufferedReader.close();
+
+        String binary = "";
+        while (n > 0) {
+            binary = n % 2 + binary;
+            n /= 2;
+        }
+
+        int consecutiveOneCount = 0;
+        int maxCount = consecutiveOneCount;
+        boolean consecutive = false;
+
+        for (int i = 0; i < binary.length() - 1; i++) {
+            if (binary.charAt(i) == '1') {
+                if (consecutive) consecutiveOneCount++;
+                else {
+                    consecutiveOneCount = 1;
+                }
+                consecutive = true;
+            } else {
+                consecutive = false;
+            }
+            maxCount = Math.max(maxCount, consecutiveOneCount);
+        }
+
+        System.out.println(consecutiveOneCount);
+    }
+
+}
