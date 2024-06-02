12b4a429c48f674f84c12870e9b47b70608e2d4d
StevenReale <smreale78@gmail.com>
2024-06-02 14:26:16 -0400

    Add description comment to Exercise 21

--- src/steven/reale/euler/EulerExercise021.java
+++ src/steven/reale/euler/EulerExercise021.java
@@ -1,5 +1,10 @@
 package steven.reale.euler;
 
+/*
+ Let d(n) be defined as the sum of proper divisors of n. Then, for some a and b where a != b, if d(a) = b and d(b) = a,
+ then a and b are called amicable numbers. Evaluate the sum of all the amicable numbers less than N.
+ */
+
 import java.util.HashMap;
 
 public class EulerExercise021 {
